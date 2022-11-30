74df809dd245a97e581e939530ca4e6046faf826
GiadaGallitto <gallitto.giada@gmail.com>
2022-11-30 15:55:55 +0100

    Add elements in for loop

--- js/script.js
+++ js/script.js
@@ -10,7 +10,11 @@
 
 const gridContainer = document.querySelector("div.grid")
 
+for(let i=0; i < 100; i++){
 
-for(let i=1; i<=100; i++){
-    
+    const newSquare = document.createElement("div");
+
+    newSquare.classList.add("square");
+
+    gridContainer.appendChild(newSquare);
 }
